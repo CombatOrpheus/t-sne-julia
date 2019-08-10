fa2bab7376c4c99164d5a02109a75842c4e0a4fb
Gabriel Oliveira <SantosGabriel28@Hotmail.com>
2019-08-10 12:21:14 -0300

    Check output

--- bhtsne.jl
+++ bhtsne.jl
@@ -1,15 +1,12 @@
 module BHTsne
 
-using ArgParse
 using Statistics: mean
-using LinearAlgebra: eigen, Symmetric
+using LinearAlgebra: eigen, Symmetric, transpose
 
 # Constants
-BH_TSNE_BIN_PATH = ifelse(
-    Sys.iswindows(),
+BH_TSNE_BIN_PATH = ifelse(Sys.iswindows(),
     joinpath(dirname(@__FILE__), "cpp/bh_tsne.exe"),
-    joinpath(dirname(@__FILE__), "cpp/bh_tsne")
-)
+    joinpath(dirname(@__FILE__), "cpp/bh_tsne"))
 
 if !isfile(BH_TSNE_BIN_PATH)
     println("Unable to find the bh_tsne binary in the same
@@ -19,47 +16,17 @@ if !isfile(BH_TSNE_BIN_PATH)
     end
 end
 
-#=
-function parse_commandline()
-    s = ArgParseSettings()
-
-    @add_arg_table s begin
-        "-d","--no_dims"
-            arg_type = Int64
-        "-p","--perplexity"
-            arg_type = Float64
-            default = 50
-        "-t","--theta"
-            arg_type = Float64
-            default = 0.5
-        "-r","--randseed"
-            arg_type = Int64
-            default = -1
-        "-n","--initial_dims"
-            arg_type = Int64
-            default = 50
-        "-v","--verbose"
-            action = :store_true
-        "-i","--input"
-            arg_type = ASCIIString
-            default = STDIN
-        "-o","--output"
-            arg_type = ASCIIString
-            default = STDOUT
-    end
-
-    parse_args(s)
-end
-=#
-
 cI32(x) = convert(Int32, x)
+rI32(x) = read(x, Int32)
+rF64(x) = read(x, Float64)
 
 function pca(X::AbstractMatrix, ndims::Integer = 50)
     (n, d) = size(X)
     (d <= ndims) && return X
     Y = X .- mean(X, dims = 1)
     C = Symmetric((Y' * Y) ./ (n - 1))
-    Ceig = eigen(C, (d-ndims+1):d) # take eigvects for top ndims largest eigvals
+    # take eigvects for top ndims largest eigvals
+    Ceig = eigen(C, (d - ndims + 1):d)
     return Y * reverse(Ceig.vectors, dims = 2)
 end
 
@@ -68,26 +35,25 @@ function bh_tsne(X, no_dims::Int = 2, initial_dims::Int = 50,
     max_iter::Int = 1000; verbose = true, use_pca = true)
 
     if use_pca
-        samples = pca(samples, initial_dims)
+        samples = pca(X, initial_dims)
     end
-    # Assume that the dimensionality of the first sample is representative for
-    # the whole batch
-    sample_count, sample_dim = size(X)
+    # Assume that the dimensionality of the first sample is representative
+    # for the whole batch
+    sample_count, sample_dim = size(samples)
 
+    # Create a temporary directory for the binary files
     mktempdir() do temp_dir
-
         open(joinpath(temp_dir, "data.dat"), "w") do data_file
-            write(
-                data_file,
+            write(data_file,
                 cI32(sample_count),
                 cI32(sample_dim),
                 theta,
                 perplexity,
                 cI32(no_dims),
-                cI32(max_iter)
-            )
-            for i in eachrow(X')
-                write(data_file, i)
+                cI32(max_iter))
+            # The executable assumes a row-major ordering for arrays
+            for i in 1:size(samples', 2)
+                write(data_file, samples'[:, i])
             end
             if randseed != -1
                 write(data_file, cI32(randseed))
@@ -98,43 +64,41 @@ function bh_tsne(X, no_dims::Int = 2, initial_dims::Int = 50,
         cd(temp_dir) do
             if verbose
                 try
-                    run(pipeline(
-                        `$BH_TSNE_BIN_PATH`,
-                        stdout = "out.txt",
-                        stderr = "errs.txt"
-                    ))
+                    run(`$BH_TSNE_BIN_PATH`)
                 catch excp
                     println(excp)
-                    error("ERROR: Call to bh_tsne exited with non-zero code exit status,
+                    error("Call to bh_tsne exited with non-zero code exit status,
                           please refer to the bh_tsne output for further detail.")
                 end
             else
                 try
-                    run(pipeline(`$BH_TSNE_path`, stdout = DevNull))
+                    run(pipeline(`$BH_TSNE_BIN_PATH`,
+                        stdout = nothing,
+                        stderr = nothing))
                 catch excp
                     println(excp)
-                    error("ERROR: Call to bh_tsne exited with a non-zero return code exit status,
+                    error("Call to bh_tsne exited with a non-zero return code exit status,
                           please enable verbose mode and refer to the bh_tsne output for further details")
                 end
             end
         end
 
         open(joinpath(temp_dir, "result.dat"), "r") do output_file
-            result_samples = read(output_file, Int32)
-            result_dims = read(output_file, Int32)
-            results = [(read(output_file, Int), read(output_file, Int)) for _ in 1:result_samples]
-            results = [(read(output_file, Int), e) for e in results]
-            sort!(results)
-            ret = Array{Float64,2}(result_dims, result_samples)
+            result_samples = rI32(output_file)
+            result_dims = rI32(output_file)
+            # Allocate an uninitialized array for results
+            results = Array{Float64,2}(undef, result_dims, result_samples)
+            read!(output_file, results)
+            results = transpose(results)
+            ret = [(rI32(output_file), results[i, :]) for i in 1:result_samples]
+            close(output_file)
+            ret = sort(ret)
             for i in 1:result_samples
-                ret[:, i] = collect(results[i][2])
+                results[i, :] .= ret[i][2]
             end
-            return ret'
+            return results
         end
     end
-
 end
-
-
 
 end  # module BHTsne
